87ef2678825f0b637cc842d1256eb3288745138e
tmscarla <scarlattitommaso@gmail.com>
2018-01-14 18:33:29 +0100

    First commit

---
+++ TokenGame.java
@@ -0,0 +1,13 @@
+
+public class TokenGame {
+	
+	/**
+	 * Placeholder
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+        System.out.println("I'm a placeholder for the repository :) code will come soon!");
+	}
+
+}
